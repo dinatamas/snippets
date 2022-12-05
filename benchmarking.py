e92d81b04a8a39ac5660a0cb75f8a7df4ccaf9f8
Tamás Dina <53911660+dinatamas@users.noreply.github.com>
2022-12-05 20:20:48 +0100

    improve benchmarking module

--- benchmarking.py
+++ benchmarking.py
@@ -1,21 +1,46 @@
-#
-# Measure the execution time of functions.
-#
-# If ENABLED is False then there is absolutely no overhead.
-#
+"""
+The timeit decorator can be used to measure the execution time of functions.
+It uses the same timing methods as the timeit standard library module.
+
+By default timing measurements are disabled and there is no runtime overhead
+by having the decorators present in your code. To enable bechmarking, set the
+ENABLE_BENCHMARKING environment variable.
+
+An added benefit compared to the timeit module is that the function that the
+timeit decorator returns mimics the original function, i.e. it can fit into
+the normal program flow perfectly. If the return value changes during
+repeated calls or if the function has some side effects, only run one test
+and one function call at a time. This gives less reliable results, but you
+can still execute your main program multiple times to repeat the measurement.
+
+The measurements are recorded in /tmp/benchmarking.log for later analysis.
+"""
+from datetime import datetime
 import functools
 import gc
-import itertools
+import inspect
+import logging
+import os
 import time
+from typing import Any, Callable
 
-ENABLED = True
+ENABLED = bool(os.getenv('ENABLE_BENCHMARKING', False))
+
+_logger = logging.getLogger('benchmarking')
+_logger.setLevel(logging.INFO)
+_formatter = logging.Formatter('%(asctime)s.%(msecs)06d -- %(message)s', '%Y-%m-%d %H:%M:%S')
+_file_handler = logging.FileHandler('/tmp/benchmarking.log')
+_file_handler.setLevel(logging.INFO)
+_file_handler.setFormatter(_formatter)
+_logger.addHandler(_file_handler)
+if ENABLED:
+    _logger.info(f'Benchmarking module loaded at {datetime.now()}')
 
 
-def _timeit(num_calls, func, *args, **kwargs):
+def _timeit(func: Callable, num_calls: int, *args, **kwargs) -> (Any, float):
     """
-    Execute the function the given number of times
-    with the provided arguments and return the result
-    and the total measured execution time.
+    Execute the function the given number of times with the provided arguments,
+    and return the (last) result along with the total measured execution time.
     """
     start_time = time.perf_counter()
     for _ in range(num_calls):
@@ -24,7 +49,12 @@ def _timeit(num_calls, func, *args, **kwargs):
     return (res, end_time - start_time)
 
 
-def timeit(func, num_tests=1, num_calls=1, disable_gc=True):
+def timeit(
+    func: Callable,
+    num_tests: int = 1,
+    num_calls: int = 1,
+    disable_gc: bool = True
+) -> Callable:
     """
     Wraps the function to perform a set of timing measurements.
 
@@ -33,22 +63,31 @@ def timeit(func, num_tests=1, num_calls=1, disable_gc=True):
       - num_calls: How many times the function is executed in a measurement.
       - disable_gc: Whether to disable garbage collection during tests.
     """
-    global ENABLED
+    if not ENABLED or num_tests == 0 or num_calls == 0:
+        return func
+
+    funcinfo = (
+        f'{func.__name__} from ({func.__module__}'
+        f':{inspect.getsourcelines(func)[1]})'
+    )
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         if disable_gc:
             gcold = gc.isenabled()
             gc.disable()
-        wrapper.time = None
+        best_measurement = float('inf')
         try:
             for _ in range(num_tests):
-                res, timing = _timeit(num_calls, func, *args, **kwargs)
-                if wrapper.time is None or timing < wrapper.time:
-                    wrapper.time = timing
+                res, measurement = _timeit(func, num_calls, *args, **kwargs)
+                best_measurement = min(best_measurement, measurement)
+            _logger.info(
+                f'{funcinfo} executed {num_calls} times in under '
+                f'{best_measurement:.6f}s in {num_tests} tests'
+            )
+            return res
         finally:
             if disable_gc and gcold:
                 gc.enable()
-        return res
 
-    return wrapper if ENABLED else func
+    return wrapper
